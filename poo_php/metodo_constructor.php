717e67c3be294593037b9b2e17ad0308a3775a0b
GuiXaadrez1 <guilhermehenrique.a.dasilva12@gmail.com>
2025-10-24 08:20:56 -0300

    Melhorandos acesso de função e method

--- poo_php/metodo_constructor.php
+++ poo_php/metodo_constructor.php
@@ -8,25 +8,44 @@
      * 
      * Remember: the method constructor always be public
      * 
-     * Observation: $this -> acess the attributes and methods of the class 
+     * Observation: 
+     * $this -> acess the attributes and methods of the class 
      * because this is a reference to the current object 
-     * 
-     * 
+     * and yet existes the method constructor and $this-> even though 
+     * don´t declere the method  but so only exist implicitly to the declarar the object the class 
+     * and call methods publics and attributes whith $this->
+     * It is also possible to create default value attributes within the class.
+     * into method constructor
      * 
      * Portuguese:
      * O método construtor foi criado para declarar atributos dinâmicos na classe
-     * lembre-se: o método construtor sempre será público
      * 
+     * Lembre-se: 
+     * o método construtor sempre será público
+     * 
+     * Observação:
+     ** $this -> acessa os atributos e métodos da classe 
+     * porque esta é uma referência ao objeto atual 
+     * e ainda existe o construtor do método e $this-> embora 
+     * não declere o método, mas apenas existe implicitamente
+     * ao declarar um objeto da class e chamar os atributos e metodos
+     * também é possível criar atributos de valor padrão dentro da class
+     * dentro do método construtor
+    
      *@param string $nombre
      *@param int $nivel_palea
      */
     class Saiyajin
     {   
         // criando um método construtor -> método mágico
-        
-        function __construct(string $nombre, int $nivel_pelea){
+    
+        function __construct(string $nombre = "Goku", int $nivel_pelea = 1500){
             $this ->nombre = $nombre;
             $this ->nivel_pelea = $nivel_pelea;
+        }
+
+        public function nivelDePelea(int $palea = 0):string{
+            return $this->nombre." tiene un nivel de pelea de: ".(string) $palea;
         }
 
         public function tranfoming():void
@@ -46,7 +65,6 @@
         }
     }
 
-
-    $Gohan = new Saiyajin("Gohan", 1500);
+    $Gohan = new Saiyajin("Gohan",3000);
 
 ?>
