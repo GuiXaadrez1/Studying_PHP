78c8fa4569656604c58c9051114d91f55707fe7d
Guixaadrez1 <guilhermehenrique.a.dasilva12@gmail.com>
2026-04-08 21:03:35 -0300

    Apenas marcando presenca

---
+++ ShellScript/Bash/basic/variablesShell.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# o comando linux set | less (mostra nossas vaiaveis de escopo global no nosso SO)
+
+# fazendo input dinâmico usando read
+
+echo "Escreva a sua idade:"
+read idade 
+
+echo "Sua idade é: $idade"
+
